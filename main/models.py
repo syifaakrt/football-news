ed1b63f9a8946be123c5e4f0dcccaf2864d9a6ba
Syifa <syifaakrt@gmail.com>
2025-09-17 14:04:56 +0700

    tutorial 3

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 import uuid
@@ -22,6 +23,7 @@ class News(models.Model):
     news_views = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.title
