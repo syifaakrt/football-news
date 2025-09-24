0579e877cc9bf0def4b48dd1112dd80bd4cce1b7
Syifa <syifaakrt@gmail.com>
2025-09-24 14:03:14 +0700

    css tutorial 4

--- main/views.py
+++ main/views.py
@@ -70,8 +70,8 @@ def create_news(request):
     context = {
         'form': form
     }
-
     return render(request, "create_news.html", context)
+
 def show_news(request, id):
     news = get_object_or_404(News, pk=id)
     news.increment_views()
@@ -114,3 +114,21 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
